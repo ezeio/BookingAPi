c6a56a8adf9ee3539768396ed32d49800e92faed
Ubuntu Eze <ezeosy.io@gmail.com>
2018-06-27 08:38:57 +0100

    added thymeleaf resolver to solve problem with reloading html content

--- src/main/java/com/booking/app/bookingapi/config/ThymeleafConfiguration.java
+++ src/main/java/com/booking/app/bookingapi/config/ThymeleafConfiguration.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
-    @Configuration
-    public class ThymeleafConfiguration {
-        @Bean
-        public ITemplateResolver defaultTemplateResolver() {
-            FileTemplateResolver resolver = new FileTemplateResolver();
-            resolver.setSuffix(".html");
-            resolver.setPrefix("path/to/your/templates");
-            resolver.setTemplateMode("HTML5");
-            resolver.setCharacterEncoding("UTF-8");
-            resolver.setCacheable(false);
-            return resolver;
-        }
+@Configuration
+public class ThymeleafConfiguration {
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(".html");
+        resolver.setPrefix("src/main/resources/templates/");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        return resolver;
     }
+}
 
